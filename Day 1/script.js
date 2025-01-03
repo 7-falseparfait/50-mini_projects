27bc707ea3537b6d01dd23b66f2a2b8682a21df3
Osuji <weezdomosuji@gmail.com>
2025-01-03 15:44:16 +0100

    Add files via upload

---
+++ Day 1/script.js
@@ -0,0 +1,19 @@
+const panels = document.querySelectorAll('.panel');
+panels[0].classList.add('active');
+
+panels.forEach((panel) => {
+  panel.addEventListener('mouseover', (event) => {
+    if (event.target === panel) {
+      removeActiveClasses();
+      panel.classList.add('active');
+    }
+  });
+  panel.addEventListener('mouseout', () => {
+    removeActiveClasses();
+    panels[0].classList.add('active');
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => panel.classList.remove('active'));
+}
